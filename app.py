24fb527e67eb16b3a03efefe61f4d5c376285268
Aengus Sinanides <lerenardinde@gmail.com>
2024-10-03 11:23:47 +0200

    Mise en place de l'API
    petit test pour voir si l'API est compatible avec python

--- app.py
+++ app.py
@@ -1,1 +1,20 @@
-import tkinter 
+import requests
+
+def get_weather(api_key, city):
+    url = f"http://api.weatherapi.com/v1/current.json?key={api_key}&q={city}&aqi=no"
+    response = requests.get(url)
+    
+    if response.status_code == 200:
+        data = response.json()
+        location = data["location"]["name"]
+        temperature = data["current"]["temp_c"]
+        condition = data["current"]["condition"]["text"]
+        print(f"Météo à {location}: {temperature}°C, {condition}")
+    else:
+        print("erreur  lors de la réccupération des données", response.status_code)
+    
+
+if __name__ == "__main__":
+    api_key = "77eb92f97e104bf6b9773753242709"
+    city = "Paris"
+    get_weather(api_key, city)
