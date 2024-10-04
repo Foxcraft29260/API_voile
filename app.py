c03f2c68f7be20339eb1bf40d6325783e62380fd
Sarah Letot <sushigacha29@gmail.com>
2024-10-04 09:55:55 +0200

    Données
    get the données

--- app.py
+++ app.py
@@ -7,9 +7,12 @@ def get_weather(api_key, city):
     if response.status_code == 200:
         data = response.json()
         location = data["location"]["name"]
-        temperature = data["current"]["temp_c"]
-        condition = data["current"]["condition"]["text"]
-        print(f"Météo à {location}: {temperature}°C, {condition}")
+        temperature = data["forecast"]["forecastday"][0]["day"]["avgtemp_c"]
+        condition = data["forecast"]["forecastday"][0]["day"]["condition"]["text"]
+        vent = data["forecast"]["forecastday"][0]["day"]["maxwind_kph"]
+        dirVent = data["forecast"]["forecastday"][0]["hour"][8]["wind_dir"]
+        print(f"Météo à {location}: {temperature}°C, {condition}, Vitesse du vent: {vent/1.852} noeuds, et le vent vient de la direction {dirVent}")
+        return {"location": location, "temperature": temperature, "condition": condition, "vent": vent}
     else:
         print("erreur  lors de la réccupération des données", response.status_code)
     
@@ -18,3 +21,4 @@ if __name__ == "__main__":
     api_key = "77eb92f97e104bf6b9773753242709"
     city = "Paris"
     get_weather(api_key, city)
+    
